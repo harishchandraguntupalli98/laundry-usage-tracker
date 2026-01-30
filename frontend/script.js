f193411c906618503d54a56369731af7da83b019
harishchandraguntupalli98 <harishchandraguntupalli98@gmail.com>
2026-01-30 15:42:54 -0600

    Front End commit

---
+++ frontend/script.js
@@ -0,0 +1,57 @@
+const API_BASE = "http://localhost:8080/api";
+
+console.log("✅ script.js loaded");
+
+function loadStatus() {
+  fetch(`${API_BASE}/status`)
+    .then(res => res.json())
+    .then(data => {
+      data.forEach(machine => {
+        const statusText = machine.inUse
+          ? `In use by ${machine.usedBy}`
+          : "Available";
+
+        if (machine.machine === "WASHER") {
+          document.getElementById("washerStatus").innerText = statusText;
+        } else if (machine.machine === "DRYER") {
+          document.getElementById("dryerStatus").innerText = statusText;
+        }
+      });
+    });
+}
+
+function useMachine(machine) {
+  const nameInput =
+    machine === "WASHER"
+      ? document.getElementById("washerName")
+      : document.getElementById("dryerName");
+
+  fetch(`${API_BASE}/use`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      machine: machine,
+      name: nameInput.value
+    })
+  })
+    .then(res => res.text())
+    .then(msg => {
+      alert(msg);
+      loadStatus();
+    });
+}
+
+function releaseMachine(machine) {
+  fetch(`${API_BASE}/release`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ machine: machine })
+  })
+    .then(res => res.text())
+    .then(msg => {
+      alert(msg);
+      loadStatus();
+    });
+}
+
+loadStatus();
